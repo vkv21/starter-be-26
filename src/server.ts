64dcd9c619672016d1f3ae0f5111c4caa29a2147
vkv21 <vivekkannangpio@gmail.com>
2026-01-06 14:18:21 +0530

    basic starter project works with frontend dev env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { type Request, type Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:5173' }));
+// or use cors() to allow all origins
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.json({ message: 'Hello World!' });
 });
 
 export default app;
