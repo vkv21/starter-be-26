fadca1121c1a252041c3d12d343fb420dac51472
vkv21 <vivekkannangpio@gmail.com>
2026-01-06 16:31:04 +0530

    clean up env vars, package.json

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -6,10 +6,30 @@ const get = (key: string, fallback?: string) => {
   return val === undefined ? fallback : val;
 };
 
+// Check if NODE_ENV is prod or dev to set the frontend url to export
+// throw error if url is not provided in env
+let frontendurl: string;
+if (get('NODE_ENV') === 'production') {
+  if (!get('PROD_FRONTEND_URL')) {
+    throw new Error(
+      'PROD_FRONTEND_URL is not defined in environment variables'
+    );
+  }
+  frontendurl = get('PROD_FRONTEND_URL')!;
+} else {
+  if (!get('DEV_FRONTEND_URL')) {
+    throw new Error('DEV_FRONTEND_URL is not defined in environment variables');
+  }
+  frontendurl = get('DEV_FRONTEND_URL')!;
+}
+
 export const PORT = Number(get('PORT', '3000'));
+
 export const NODE_ENV = get('NODE_ENV', 'development') as
   | 'development'
   | 'production'
   | 'test';
 
-export default { PORT, NODE_ENV };
+export const FRONTEND_URL = frontendurl;
+
+export default { PORT, NODE_ENV, FRONTEND_URL };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express, { type Request, type Response } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+
+import { FRONTEND_URL } from './envConfig.js';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
-// or use cors() to allow all origins
+app.use(helmet());
+
+app.use(cors({ origin: FRONTEND_URL }));
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
